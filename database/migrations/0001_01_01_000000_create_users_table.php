27176d8f16c1a22749cbff7a4f46f9504c8fc0cb
Abhishek Solanki <abhishek.solanki@brainwaveinfoways.com>
2025-06-27 12:42:32 +0530

    minor changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -44,6 +44,6 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('');
     }
 };
